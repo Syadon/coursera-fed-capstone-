ba008bbc1f8f955d5e5dcce5afcf2846cc272b67
Syadon <>
2023-04-02 16:00:29 +0200

    Setting up the HTML document

---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav>
+            <img src="" alt="Logo" width="100" height="100"/>
+            <ul>
+                <li>
+                    <a href="">Link 1</a>
+                </li>
+                <li>
+                    <a href="">Link 2</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
